7eb7cb2a0858d2d63d4cf7690451b84d33dd5721
forukotan <forukotan1@gmail.com>
2022-07-20 10:29:45 -0500

    area excercise

---
+++ operators2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace operators2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the radius");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = Area(radius);
+            Console.WriteLine($"The are of a circle with radius {radius} is {areaOfCircle}");
+        }
+        public static double Area(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
